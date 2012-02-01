2064de82b0ae3117134be86e4ab27b91672c05b4
Hernan Chanfreau <hchanfreau@gmail.com>
2012-02-01 02:49:50 +0000

    Bioplat Application 2.3.0 release

---
+++ R4J/src/main/java/edu/unlp/medicine/r4j/core/R4J.java
@@ -0,0 +1,74 @@
+package edu.unlp.medicine.r4j.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main class of the bridge. You can execute files in an independent way
+ * (executeFile) or you can get a session for keeping the state of the variables
+ * (getSession).
+ * 
+ * @author Matias Butti
+ * 
+ */
+public class R4J {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(R4J.class);
+
+	/**
+	 * Execute a file with R scripts.
+	 * 
+	 * @param userFolderName
+	 * @param filePathToExecuteWhenStart
+	 * @throws RException
+	 */
+	public void executeFile(String userFolderName, String filePathToExecuteWhenStart) throws RException {
+		R4JSession r4JSession = new R4JSession(userFolderName);
+		r4JSession.addStatementsOfTheFile(filePathToExecuteWhenStart);
+		r4JSession.flush();
+		r4JSession.close();
+	}
+
+	/**
+	 * It creates a clean RSession.
+	 * 
+	 * @param path
+	 *            The path of the temporal file to write the scripts of the
+	 *            session.
+	 * @return It returns the session.
+	 * @throws RException
+	 *             If there is any I/O problem trying to create the file the
+	 *             method will throw an RException and the session will not be
+	 *             created.
+	 */
+	public R4JSession getRSession(String userFolderName) throws RException {
+		try {
+			return new R4JSession(userFolderName);
+		} catch (RException e) {
+			LOGGER.error(e.getMessage());
+			throw e;
+		}
+	}
+
+	/**
+	 * 
+	 * @param sessionName
+	 *            The name of the session. It will be used as the folder name.
+	 * @param path
+	 *            The absolute path in which the folder with the session name
+	 *            will be created. This is used in case you want to use your own
+	 *            folder to keep the results instead of using the default r4j
+	 *            folder.
+	 * @return
+	 * @throws RException
+	 */
+	public R4JSession getRSession(String sessionName, String path) throws RException {
+		try {
+			return new R4JSession(sessionName, path);
+		} catch (RException e) {
+			LOGGER.error(e.getMessage());
+			throw e;
+		}
+	}
+
+}
---
+++ R4J/src/main/java/edu/unlp/medicine/r4j/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package edu.unlp.medicine.r4j.utils;
+import edu.unlp.medicine.r4j.constants.OSDependentConstants;
+
+public class StringUtils {
+	
+	public static String addQuotes(String string){
+		return OSDependentConstants.DOUBLE_QUOTE + string + OSDependentConstants.DOUBLE_QUOTE;
+	}
+
+	
+	
+}
