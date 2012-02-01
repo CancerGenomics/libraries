1a6d93044553bbbef4cfc2d6b6ec90d0512abe67
Hernan Chanfreau <hchanfreau@gmail.com>
2012-02-01 02:53:00 +0000

    Bioplat GRID 0.0.1 final release

---
+++ BioPlatGRID/src/main/java/edu/unlp/medicine/grid/core/SumJob.java
@@ -0,0 +1,33 @@
+package edu.unlp.medicine.grid.core;
+
+import java.util.List;
+
+import org.gridgain.grid.GridException;
+import org.gridgain.grid.GridJobAdapter;
+
+import edu.unlp.medicine.r4j.core.R4JFactory;
+import edu.unlp.medicine.r4j.core.R4JSession;
+import edu.unlp.medicine.r4j.core.RException;
+
+public class SumJob extends GridJobAdapter<Integer> {
+	private int number;
+	
+	public SumJob(final int i) {
+		this.number = i;
+	}
+
+	@Override
+	public Object execute() throws GridException {
+		String recta="recta";
+		
+		try {
+			R4JSession rj4Session = R4JFactory.getR4JInstance().getRSession("testGetValue" + number );
+			rj4Session.assign(recta, "c(1,2,3,4)");
+			List<String> result = rj4Session.getArrayValue(recta);
+			rj4Session.close();
+			return new Integer(result.get(0));
+		} catch (RException e) {
+			return 0;
+		}
+	}
+}
