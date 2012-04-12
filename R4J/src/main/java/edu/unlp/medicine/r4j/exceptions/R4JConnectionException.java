ee22aaa54f6013ac9c033c66f4bc9a95dd29deae
Deigo Garcia <dgarcia@doesnotexist.com>
2012-04-12 01:11:36 +0000

    R4J - Puente para Rserve.

---
+++ R4J/src/main/java/edu/unlp/medicine/r4j/exceptions/R4JConnectionException.java
@@ -0,0 +1,8 @@
+package edu.unlp.medicine.r4j.exceptions;
+
+public class R4JConnectionException extends Exception {
+	public R4JConnectionException(final String message, final Throwable error) {
+		super(message, error);
+	}
+
+}
---
+++ R4J/src/main/java/edu/unlp/medicine/r4j/transformers/IR4JvalueTransformer.java
@@ -0,0 +1,16 @@
+package edu.unlp.medicine.r4j.transformers;
+
+import org.rosuda.REngine.REXP;
+
+import edu.unlp.medicine.r4j.values.R4JValue;
+
+/**
+ * This interface represents the transformation of an REXP to a R4JValue
+ * @author Diego García
+ *
+ */
+public interface IR4JvalueTransformer {
+	
+	R4JValue transform (final REXP source);
+
+}
