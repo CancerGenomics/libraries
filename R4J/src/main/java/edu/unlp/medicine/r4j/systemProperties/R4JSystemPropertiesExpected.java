03bcd79984cef81dd8ff73669494fa503c19f8af
Matias Butti <matias.butti@gmail.com>
2013-07-13 03:38:48 +0000

    Big refactor de R4J. Orden y limpieza.

---
+++ R4J/src/main/java/edu/unlp/medicine/r4j/systemProperties/R4JSystemPropertiesExpected.java
@@ -0,0 +1,11 @@
+package edu.unlp.medicine.r4j.systemProperties;
+
+public class R4JSystemPropertiesExpected {
+
+	public static String R_REQUIRED_LIBRARIES_FILE_PATH_PROPERTY = "R_REQUIRED_LIBRARIES_FILE_PATH";
+	public static String R_HOME_BIOPLAT_PROPERTY = "R_HOME_BIOPLAT";
+	public static String SCRIPT_LOG_FILE_PATH_PROPERTY = "SCRIPT_LOG_FILE_PATH";
+	
+	
+
+}
