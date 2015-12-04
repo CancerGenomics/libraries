c3c6903542678b6e8464c087a51ada3c2b3fbc4d
Diego Ariel Martínez <diegoariel83@gmail.com>
2015-12-03 23:38:34 -0300

    R_LOG como constante

--- R4J/src/main/java/edu/unlp/medicine/r4j/systemProperties/R4JSystemPropertiesExpected.java
+++ R4J/src/main/java/edu/unlp/medicine/r4j/systemProperties/R4JSystemPropertiesExpected.java
@@ -5,7 +5,7 @@ public class R4JSystemPropertiesExpected {
 	public static final String R_REQUIRED_LIBRARIES_FILE_PATH_PROPERTY = "R_REQUIRED_LIBRARIES_FILE_PATH";
 	public static final String R_HOME_BIOPLAT_PROPERTY = "R_HOME_BIOPLAT";
 	public static final String SCRIPT_LOG_FILE_PATH_PROPERTY = "SCRIPT_LOG_FILE_PATH";
-	public static String R_LOG = "rlog";
+	public static final String R_LOG = "rlog";
 	
 
 }
